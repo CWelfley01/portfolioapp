f98052acf3866b3cbcaf2473b933d977740a5f5c
cwelfley01 <cwelfley01@gmail.com>
2022-05-18 23:29:04 -0400

    added functions

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -14,7 +14,7 @@ export default class PortfolioContainer extends Component {
         const data = ["Quip", "Eventbrite", "Ministry Safe","Swing Away"];
 
         return data.map(item => {
-            return <PortfolioItem title={item}  />;
+            return <PortfolioItem title={item} url={"google.com"} />;
         })
     }
 
