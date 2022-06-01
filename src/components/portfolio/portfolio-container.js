1eb062c441f58453a9dc1ec6d40c10a6916a63be
cwelfley01 <cwelfley01@gmail.com>
2022-05-31 22:33:29 -0400

    no match

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,11 +8,12 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: "Welcome to my portfolio",
+            isLoading: false,
             data: [
-            { title: "Quip", category: "eCommerce" },
-            { title: "Eventbrite", category: "Scheduling" },
-            { title: "Ministry Safe", category: "Enterprise" },
-            { title: "SwingAway", category: "eCommerce" }
+            { title: "Secure Recycling", category: "Electronic Recycling", slug: "Secure_Recycling" },
+            { title: "Eventbrite", category: "Scheduling", slug:"Eventbrite" },
+            { title: "Ministry Safe", category: "Enterprise", slug: "MinistrySafe" },
+            { title: "SwingAway", category: "eCommerce", slug: "SwingAway" }
             ]
         };
         
@@ -30,7 +31,7 @@ export default class PortfolioContainer extends Component {
     
     portfolioItems() {
         return this.state.data.map(item => {
-            return <PortfolioItem key={item.title} title={item.title} url={"google.com"} />;
+            return <PortfolioItem key={item.title} title={item.title} url={"google.com"} slug={item.slug} />;
         })
     }
 
@@ -41,6 +42,9 @@ export default class PortfolioContainer extends Component {
     // }
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>
+        }
         return(
             <div>
                 <h2>{this.state.pageTitle}</h2>
