9baaeba828d6b2572d59dffe398cada7b243bd4c
cwelfley01 <cwelfley01@gmail.com>
2022-06-06 23:35:19 -0400

    update6622-3

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom"
 
 
 
-export default function(props) {
+export default class PortfolioItems extends Component{
+   constructor(props) {
+       super(props);
+
+       this.state={ portfolioItemClass:""};
+   }
+
+   handleMouseEnter(){
+    this.setState({portfolioItemClass:"image-blur"});
+   }
+
+   handleMouseLeave(){
+    this.setState({portfolioItemClass:""});
+   }
+
+   render(){ 
+    const {id,description, thumb_image_url, logo_url} = this.props.item;
     
-    const {id,description, thumb_image_url, logo_url} = props.item;
     return (
-        <div className="portfolio-item-wrapper">
+        <div className="portfolio-item-wrapper" 
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+        >
+            
             <div
-            className="portfolio-img-background"
+            className={"portfolio-img-background " + this.state.portfolioItemClass}
             style={{backgroundImage:"url("+ thumb_image_url+")"
             
             }}
@@ -25,4 +44,5 @@ export default function(props) {
             </div>
         </div>
     );
+    }
 }
