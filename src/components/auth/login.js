f0ab0e0c4237ca0a57cb335c0f88bdbfa516f988
cwelfley01 <cwelfley01@gmail.com>
2022-06-07 23:47:02 -0400

    update6722-1

---
+++ src/components/auth/login.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+
+
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>login to access your dashboard</h1>
+        
+        
+       <form>
+           <input type="text"/>
+           <input type="password"/>
+       </form>
+
+      </div>
+    )
+  }
+}
