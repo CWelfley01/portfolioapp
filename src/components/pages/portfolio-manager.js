50a1e6ccaf4456dcde4b1062f720d308b7bd956c
cwelfley01 <cwelfley01@gmail.com>
2022-06-14 22:31:20 -0400

    updates

---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../portfolio/portfolio-form';
+
+
+
+export default class PortfolioManager extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+
+  }
+
+
+  handleSuccessfulFormSubmission(portfolioItem){
+    portfolioItem: [portfolioItem].concat(this.state.portfolioItems);
+  }
+
+  handleFormSubmissionError(error){
+    console.log("handleFormSubmissionError",error);
+  }
+
+
+
+  getPortfolioItems(){
+    axios.get("https://chriswelfley.devcamp.space/portfolio/portfolio_items?oder_by=created_at&direction=desc", { 
+      withCredentials: true
+    }).then(response => {
+      this.setState({
+        portfolioItems: [...response.data.portfolio_items]
+      });
+    }).catch(err => {
+      console.log("error in get portfolio items", error);
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <PortfolioForm 
+          handleSuccessfulFormSubmission ={this.handleSuccessfulFormSubmission}
+          handleFormSubmissionError ={this.handleFormSubmissionError}          
+          />
+        </div>
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems}/>
+        </div>
+
+        
+      </div>
+    )
+  }
+}
