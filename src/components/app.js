34bf61ffd6da95b6996482512f36f4e142c0565b
cwelfley01 <cwelfley01@gmail.com>
2022-06-23 23:51:23 -0400

    modal stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route }  from "react-router-dom";
 import axios from 'axios';
-import { library } from "@fortawesome/fontawesome-svg-core";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt, faEdit} from "@fortawesome/free-solid-svg-icons";
+import Icons from "../helpers/icons";
 
 import NavigationContainer from './navigation/navigation-container';
 import Home from "./pages/home";
@@ -16,10 +16,12 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt, faEdit);
+
 export default class App extends Component {
   constructor(props){
     super(props);
+
+    Icons();
 
     this.state = {
       loggedInStatus:"NOT_LOGGED_IN"
@@ -115,7 +117,13 @@ export default class App extends Component {
               />
              <Route path="/about-me" component={About}/>
              <Route path="/contact" component={Contact}/>
-             <Route path="/blog" component={Blog}/>
+             <Route path="/blog" 
+             render={props => (
+
+              <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
+             )}
+             
+             />
              <Route path="/b/:slug" component={BlogDetail}/>
              {this.state.loggedInStatus === "LOGGED_IN" ? (
               this.authorizedPages()
