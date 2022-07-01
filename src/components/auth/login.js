3d86997b80892800872465be6f7a489d3387f27b
cwelfley01 <cwelfley01@gmail.com>
2022-06-30 20:40:20 -0400

    functionally finished

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,103 +1,94 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-
-
-
-
-
+import React, { Component } from "react";
+import axios from "axios";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
-      
+
     this.state = {
-      email:"",
-      password:"",
-      errorText:""    
-    }
+      email: "",
+      password: "",
+      errorText: "",
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
-
   }
-  handleChange(event){
+  handleChange(event) {
     this.setState({
-      [event.target.name]:event.target.value,
-      errorText: ""
+      [event.target.name]: event.target.value,
+      errorText: "",
     });
   }
 
-
-
-  handleSubmit(event){
-    axios.post("https://api.devcamp.space/sessions",
-      {
-      client: {
-        email: this.state.email,
-      password: this.state.password
-      }
-    
-    },
-      { withCredentials: true }      
-    )
-      .then(response =>{
-      if (response.data.status ==='created'){
-        this.props.handleSuccessfulAuth();
-      }else{
-        this.setState({
-          errorText: "wrong email or password"
-        });
-        this.props.handleUnsuccessfulAuth();
-      }
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
+        {
+          client: {
+            email: this.state.email,
+            password: this.state.password,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        if (response.data.status === "created") {
+          this.props.handleSuccessfulAuth();
+        } else {
+          this.setState({
+            errorText: "wrong email or password",
+          });
+          this.props.handleUnsuccessfulAuth();
+        }
       })
-      .catch(error =>{
+      .catch((error) => {
         console.log("some error occurred", error);
         this.setState({
-          errorText: "an error occurred"
+          errorText: "an error occurred",
         });
         this.props.handleUnsuccessfulAuth();
       });
-  
+
     event.preventDefault();
   }
-    
-
-
 
   render() {
     return (
       <div>
         <h1>Login to Access Your Dashboard</h1>
         <div>{this.state.errorText}</div>
-        
-       <form onSubmit={this.handleSubmit}>
-           <input 
-            type="email"
-            name="email"
-            placeholder="your email"
-            value={this.state.email}
-            onChange={this.handleChange}
-           />
 
+        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+          <div className="form-group">
+          <FontAwesomeIcon icon="envelope" />
+            <input
+              type="email"
+              name="email"
+              placeholder="your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
 
-          <input 
-            type="password"
-            name="password"
-            placeholder="your password"
-            value={this.state.password}
-            onChange={this.handleChange}
-           />
+          <div className="form-group">
+          <FontAwesomeIcon icon="lock" />
+            <input
+              type="password"
+              name="password"
+              placeholder="your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
 
-           <div>
-            <button type="submit">Login</button>
-               
-            </div>
-
-
-       </form>
-
+          
+          <button className="btn" type="submit">Login</button>
+          
+        </form>
       </div>
-    )
+    );
   }
 }
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -24,6 +24,26 @@ class Blog extends Component {
     this.handleSuccessfullNewBlogSubmission = this.handleSuccessfullNewBlogSubmission.bind(
       this
     );
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleDeleteClick(blog) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        this.setState({
+          blogItems: this.state.blogItems.filter((blogItem) => {
+            return blog.id !== blogItem.id;
+          }),
+        });
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("delete blog error", error);
+      });
   }
 
   handleSuccessfullNewBlogSubmission(blog) {
@@ -95,7 +115,18 @@ class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map((blogItem) => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      if (this.props.loggedInStatus === "LOGGED_IN") {
+        return (
+          <div className="admin-blog-wrapper">
+            <BlogItem key={blogItem.id} blogItem={blogItem} />
+            <a onClick={() => this.handleDeleteClick(blogItem)}>
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
+        );
+      } else {
+        return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      }
     });
 
     return (
@@ -107,13 +138,13 @@ class Blog extends Component {
           handleModalClose={this.handleModalClose}
           modalIsOpen={this.state.blogModalIsOpen}
         />
-        {this.props.loggedInStatus === "LOGGED_IN" ?(
-        <div className="new-blog-link">
-          <a onClick={this.handleNewBlogClick}>
-            <FontAwesomeIcon icon="plus-circle" />
-          </a>
-        </div>
-        ): null}
+        {this.props.loggedInStatus === "LOGGED_IN" ? (
+          <div className="new-blog-link">
+            <a onClick={this.handleNewBlogClick}>
+              <FontAwesomeIcon icon="plus-circle" />
+            </a>
+          </div>
+        ) : null}
         <div className="content-container">{blogRecords}</div>
 
         {this.state.isLoading ? (
